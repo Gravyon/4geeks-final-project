2957fb61b3d94810d9fb9203cc80aa2eae625225
hermanbayron <foforito2001gg@gmail.com>
2022-10-26 20:41:55 +0000

    Hice el map para las cards

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -30,7 +30,7 @@ const injectContext = (PassedComponent) => {
        * you should do your ajax requests or fetch api requests here. Do not use setState() to save data in the
        * store, instead use actions, like this:
        **/
-      // <---- calling this function from the flux.js actions
+      state.actions.getProduct(); // <---- calling this function from the flux.js actions
     }, []);
 
     // The initial value for the context is not null anymore, but the current state of this component,
