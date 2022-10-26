2b246db8be3fae9341d041520ad8a009b0574555
aucapricornus <rdjmartinez95@gmail.com>
2022-10-26 14:43:11 +0000

    comentario

--- src/api/models.py
+++ src/api/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
+#hola
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
