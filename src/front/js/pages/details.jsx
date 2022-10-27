ba086959f716d331656ead7645ca3e761dd1bb08
lauramagallanes <pasocenturiontours@gmail.com>
2022-10-27 14:59:03 +0000

    Comienzo a agregar funcionalidad de cambiar de producto con slider

--- src/front/js/pages/details.jsx
+++ src/front/js/pages/details.jsx
@@ -10,22 +10,33 @@ import Form from "react-bootstrap/Form";
 export const ProductDetail = (props) => {
   const { store, actions } = useContext(Context);
   const params = useParams();
+  let [index, setIndex] = useState(0);
+
 
   useEffect(() => {
     actions.getProductDetail(params.id);
-  }, []);
+  }, [selectedIndex]);
+
+  let handleSelect = (selectedIndex, e) => {
+    const products = actions.getProduct
+    console.log(products)
+    setIndex(selectedIndex);
+
+  };
+
+
 
   return (
     <div className="container mt-5">
       <div className=" col-9 central-content d-flex justify-content-center">
-        <Carousel variant="dark">
+      {store.product.map((item, index) => (<Carousel activeIndex={index} onSelect={handleSelect} variant="dark">
           <Carousel.Item>
             <div className="row">
               <div className="col-5">
                 <Card style={{ width: "18rem", height: "400px" }}>
                   <Card.Img variant="top" src="" />
                   <Card.Body>
-                    <Card.Title>{store.productDetail?.name}</Card.Title>
+                    <Card.Title>{item?.name}</Card.Title>
                     <Card.Text>Price $</Card.Text>
                     <Button variant="primary">Add to cart</Button>
                   </Card.Body>
@@ -34,14 +45,14 @@ export const ProductDetail = (props) => {
               <div className="col-7">
                 <Card style={{ width: "18rem", height: "400px" }}>
                   <Card.Body>
-                    <Card.Title>{store.productDetail?.name}</Card.Title>
+                    <Card.Title>{item?.name}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">
-                      {store.productDetail?.category}
+                      {item?.category}
                     </Card.Subtitle>
                     <Card.Text>Detalles de la obra</Card.Text>
                     <Card.Link href="#">Card Link</Card.Link>
                     <Card.Link href="#">
-                      Price: $ {store.productDetail?.price}
+                      Price: $ {item?.price}
                     </Card.Link>
                   </Card.Body>
                 </Card>
@@ -62,7 +73,7 @@ export const ProductDetail = (props) => {
               </form>
             </div>
           </Carousel.Item>
-        </Carousel>
+        </Carousel>))}
       </div>
       <div className="col-3 nav"></div>
       <div className="col-9 comments"></div>
