8cfde533ba808fffe0a2a381aa15974318d5d0cc
aucapricornus <rdjmartinez95@gmail.com>
2022-10-27 20:03:04 +0000

    Reformateo landing-page

--- src/front/js/component/landing-page.jsx
+++ src/front/js/component/landing-page.jsx
@@ -10,30 +10,18 @@ export const LandingPage = () => {
 
   return (
     <div>
-      {" "}
       {store.product.map((item, id) => (
         <Card key={id} style={{ width: "18rem" }}>
-          {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
           <Card.Body>
-            <img
-              src={
-                "https://uruguayeduca.anep.edu.uy/sites/default/files/2018-05/Imagen%20del%20recurso_0.jpg"
-                // +
-                // (id + 1) +
-                // ".jpg"
-              }
-              className="card-img-top"
-              alt="..."
-            />
-            <Card.Title>{item.name}</Card.Title>
-            <Card.Text>{item.category}</Card.Text>
-            <Card.Text>{item.price}</Card.Text>
-            <Link to={"/product-detail" + (id + 1)} className="btn btn-primary">
-              Leer mas...
-            </Link>
+            <img src={"https://uruguayeduca.anep.edu.uy/sites/default/files/2018-05/Imagen%20del%20recurso_0.jpg"}className="card-img-top"alt="..."/>
+            <Card.Title> {item.name}</Card.Title>
+            <Card.Text> {item.category}</Card.Text>
+            <Card.Text> {item.price}</Card.Text>
+            <Link to={"/product-detail/" + (id + 1)} className="btn btn-primary">Leer mas...</Link>
+            <button type="button" class="btn btn-primary mx-1">Comprar ahora</button>
           </Card.Body>
         </Card>
-      ))}{" "}
+      ))}
     </div>
   );
 };
