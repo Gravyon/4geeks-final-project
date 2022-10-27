4c1d241b0982422f1325ef8021cba569e48e0923
aucapricornus <rdjmartinez95@gmail.com>
2022-10-27 20:25:37 +0000

    Boton agregar producto hecho

--- src/front/js/component/landing-page.jsx
+++ src/front/js/component/landing-page.jsx
@@ -13,12 +13,12 @@ export const LandingPage = () => {
       {store.product.map((item, id) => (
         <Card key={id} style={{ width: "18rem" }}>
           <Card.Body>
-            <img src={"https://uruguayeduca.anep.edu.uy/sites/default/files/2018-05/Imagen%20del%20recurso_0.jpg"}className="card-img-top"alt="..."/>
+            <img src={"https://uruguayeduca.anep.edu.uy/sites/default/files/2018-05/Imagen%20del%20recurso_0.jpg"}className="img-fluid"alt="..."/>
             <Card.Title> {item.name}</Card.Title>
             <Card.Text> {item.category}</Card.Text>
             <Card.Text> {item.price}</Card.Text>
             <Link to={"/product-detail/" + (id + 1)} className="btn btn-primary">Leer mas...</Link>
-            <button type="button" class="btn btn-primary mx-1">Comprar ahora</button>
+            <button type="button" onClick={()=>actions.getProducts(item.name)} class="btn btn-primary mx-1">Comprar ahora</button>
           </Card.Body>
         </Card>
       ))}
