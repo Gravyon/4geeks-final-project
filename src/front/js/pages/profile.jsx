acc16742551eb77d3792b41dfcbaa7a4833b5aa2
lauramagallanes <pasocenturiontours@gmail.com>
2022-10-27 20:04:35 +0000

    Actualizacion del archivo profile, agregando contenido hardcodeado para ver como queda

--- src/front/js/pages/profile.jsx
+++ src/front/js/pages/profile.jsx
@@ -6,6 +6,8 @@ import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Row from "react-bootstrap/Row";
 import Tab from "react-bootstrap/Tab";
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
 
 export const Profile = (props) => {
   const { store, actions } = useContext(Context);
@@ -24,17 +26,40 @@ export const Profile = (props) => {
             <Col sm={3}>
               <Nav variant="pills" className="flex-column">
                 <Nav.Item>
-                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                  <Nav.Link eventKey="first">Your info</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                  <Nav.Link eventKey="second">Order history</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Your favorites</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Preferences</Nav.Link>
                 </Nav.Item>
               </Nav>
             </Col>
             <Col sm={9}>
               <Tab.Content>
-                <Tab.Pane eventKey="first"></Tab.Pane>
-                <Tab.Pane eventKey="second"></Tab.Pane>
+                <Tab.Pane eventKey="first">
+                  <div>
+                    <h4>Username: user1</h4>
+                    <h4>Email: 123</h4>
+                    <h4>Password: ********</h4>
+                    <Button variant="warning">Delete account</Button>
+                  </div>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <div>
+                    <ListGroup>
+                      <ListGroup.Item>Order number 1</ListGroup.Item>
+                      <ListGroup.Item>Order number 2</ListGroup.Item>
+                      <ListGroup.Item>Order number 3</ListGroup.Item>
+                      <ListGroup.Item>Order number 4</ListGroup.Item>
+                      <ListGroup.Item>Order number 5</ListGroup.Item>
+                    </ListGroup>
+                  </div>
+                </Tab.Pane>
               </Tab.Content>
             </Col>
           </Row>
