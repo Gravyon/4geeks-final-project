670fc466af0e33bff82aaa54dae60f8639a26dd1
hermanbayron <foforito2001gg@gmail.com>
2022-10-27 19:48:49 +0000

    Sign up completitopero sin fetch

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,21 +1,18 @@
-import React, {Component} from "react";
-import {useState, useEffect, useContext} from "react";
-import {Link} from "react-router-dom";
-import {Context} from "../store/appContext";
-import {useHistory} from "react-router-dom";
+import React, { Component } from "react";
+import { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { Login } from "./pages/login.jsx";
-import { SignUp } from "./pages/signup.jsx";
 
 export const NavbarPrincipal = () => {
-    const {store, actions} = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-<<<<<<< HEAD
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg">
@@ -32,19 +29,22 @@ export const NavbarPrincipal = () => {
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="#action2">Products</Nav.Link>
               <Nav.Link href="#action2">Contact</Nav.Link>
+              {/* <Nav.Link href="/login">Login</Nav.Link> */}
               <Link to={"/login"}>
+                {" "}
                 {!store.auth ? (
                   <button type="button" className="btn btn-primary">
                     Login
                   </button>
-                ) : null}
+                ) : null}{" "}
               </Link>
               <Link to={"/signup"}>
+                {" "}
                 {!store.auth ? (
                   <button type="button" className="btn btn-primary">
                     Sign Up
                   </button>
-                ) : null}
+                ) : null}{" "}
               </Link>
               <NavDropdown title="Link" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3"></NavDropdown.Item>
@@ -71,50 +71,4 @@ export const NavbarPrincipal = () => {
       </Navbar>
     </div>
   );
-=======
-    return (<div>
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Container fluid>
-                <Navbar.Brand href="/">Icono de Casa Arte</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll"/>
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav className="me-auto my-2 my-lg-0"
-                        style={
-                            {maxHeight: "100px"}
-                        }
-                        navbarScroll>
-                        <Nav.Link href="#action2">Favorites</Nav.Link>
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="#action2">Products</Nav.Link>
-                        <Nav.Link href="#action2">Contact</Nav.Link>
-                        <Link to={"/login"}> {
-                            !store.auth ? (<button type="button" className="btn btn-primary">
-                                Login
-                            </button>) : null
-                        } </Link>
-                        <Link to={"/signup"}> {
-                            !store.auth ? (<button type="button" className="btn btn-primary">
-                                Sign In
-                            </button>) : null
-                        } </Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3"></NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item href="#action5">
-                                Something else here
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    <Form className="d-flex">
-                        <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search"/>
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    </div>);
->>>>>>> 5489a4325f35e6dd8b72cc7f65344e3d70f32e06
 };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
@@ -13,78 +12,36 @@ import { SignUp } from "./pages/signup.jsx";
 
 import { NavbarPrincipal } from "./component/navbar.jsx";
 import { Footer } from "./component/footer.jsx";
-=======
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
-
-import {Home} from "./pages/home.jsx";
-import {Demo} from "./pages/demo";
-import {Single} from "./pages/single";
-import injectContext from "./store/appContext";
-import {ProductDetail} from "./pages/details.jsx";
-import {Login} from "./pages/login.jsx";
-import {SignUp} from "./pages/signup.jsx";
-
-import {NavbarPrincipal} from "./component/navbar.jsx";
-import {Footer} from "./component/footer.jsx";
->>>>>>> 5489a4325f35e6dd8b72cc7f65344e3d70f32e06
 
 // create your first component
 const Layout = () => {
-<<<<<<< HEAD
-  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // the basename is used when your project is published in a subdirectory and not in the root of the domain
   // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
   const basename = process.env.BASENAME || "";
 
   return (
     <div>
+      {" "}
       <BrowserRouter basename={basename}>
+        {" "}
         <ScrollToTop>
-          <NavbarPrincipal />
+          {" "}
+          <NavbarPrincipal />{" "}
           <Routes>
-            <Route element={<Home />} path="/" />
-            <Route element={<Login />} path="/login" />
-            <Route element={<SignUp />} path="/signup" />
-            <Route element={<Demo />} path="/demo" />
-            <Route element={<Single />} path="/single/:theid" />
-            <Route element={<ProductDetail />} path="/product-detail/:id" />
-            <Route element={<h1> Not found! </h1>} />
+            {" "}
+            <Route element={<Home />} path="/" />{" "}
+            <Route element={<Login />} path="/login" />{" "}
+            <Route element={<SignUp />} path="/signup" />{" "}
+            <Route element={<Demo />} path="/demo" />{" "}
+            <Route element={<Single />} path="/single/:theid" />{" "}
+            <Route element={<ProductDetail />} path="/product-detail/:id" />{" "}
+            <Route element={<h1>Not found !</h1>} />{" "}
           </Routes>{" "}
-          <Footer />
+          <Footer />{" "}
         </ScrollToTop>{" "}
       </BrowserRouter>{" "}
     </div>
   );
-=======
-    // the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
-
-    return (<div> {" "}
-        <BrowserRouter basename={basename}> {" "}
-            <ScrollToTop> {" "}
-                <NavbarPrincipal/>{" "}
-                <Routes> {" "}
-                    <Route element={<Home/>}
-                        path="/"/>{" "}
-                    <Route element={<Login/>}
-                        path="/login"/>{" "}
-                    <Route element={<SignUp/>}
-                        path="/signup"/>{" "}
-                    <Route element={<Demo/>}
-                        path="/demo"/>{" "}
-                    <Route element={<Single/>}
-                        path="/single/:theid"/>{" "}
-                    <Route element={<ProductDetail/>}
-                        path="/product-detail/:id"/>{" "}
-                    <Route element={
-                        <h1>Not found !</h1>
-                    }/>{" "} </Routes>
-                {" "}
-                <Footer/>{" "} </ScrollToTop>
-            {" "} </BrowserRouter>
-        {" "} </div>);
->>>>>>> 5489a4325f35e6dd8b72cc7f65344e3d70f32e06
 };
 
 export default injectContext(Layout);
