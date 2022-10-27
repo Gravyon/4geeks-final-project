94612a2d9b912dd93fd6d79259ed5e75bdae5b46
lauramagallanes <pasocenturiontours@gmail.com>
2022-10-27 17:20:37 +0000

    sigo trabajando en la funcionalidad de cambio de producto en el slider

--- src/front/js/pages/details.jsx
+++ src/front/js/pages/details.jsx
@@ -12,68 +12,70 @@ export const ProductDetail = (props) => {
   const params = useParams();
   let [index, setIndex] = useState(0);
 
-
   useEffect(() => {
     actions.getProductDetail(params.id);
-  }, [selectedIndex]);
+  }, [setIndex]);
 
   let handleSelect = (selectedIndex, e) => {
-    const products = actions.getProduct
-    console.log(products)
+    selectedIndex = actions.getProductDetail(params.id + 1);
+    // const products = actions.getProduct;
+    // console.log(products);
     setIndex(selectedIndex);
-
   };
-
-
 
   return (
     <div className="container mt-5">
       <div className=" col-9 central-content d-flex justify-content-center">
-      {store.product.map((item, index) => (<Carousel activeIndex={index} onSelect={handleSelect} variant="dark">
-          <Carousel.Item>
-            <div className="row">
-              <div className="col-5">
-                <Card style={{ width: "18rem", height: "400px" }}>
-                  <Card.Img variant="top" src="" />
-                  <Card.Body>
-                    <Card.Title>{item?.name}</Card.Title>
-                    <Card.Text>Price $</Card.Text>
-                    <Button variant="primary">Add to cart</Button>
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="col-7">
-                <Card style={{ width: "18rem", height: "400px" }}>
-                  <Card.Body>
-                    <Card.Title>{item?.name}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">
-                      {item?.category}
-                    </Card.Subtitle>
-                    <Card.Text>Detalles de la obra</Card.Text>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">
-                      Price: $ {item?.price}
-                    </Card.Link>
-                  </Card.Body>
-                </Card>
-              </div>
-            </div>
-            <div className="input-container">
-              <form action="">
-                <Form.Label>Comment</Form.Label>
-                <div className="row">
-                  <Form.Control
-                    type="text"
-                    placeholder="Leave your comments here please"
-                  />
-                  <Button variant="primary" type="submit">
-                    Submit
-                  </Button>
+        {store.product.map((item, index) => (
+          <Carousel
+            activeIndex={index}
+            onSelect={handleSelect(index)}
+            variant="dark"
+            key={item.id}
+          >
+            <Carousel.Item>
+              <div className="row">
+                <div className="col-5">
+                  <Card style={{ width: "18rem", height: "400px" }}>
+                    <Card.Img variant="top" src="" />
+                    <Card.Body>
+                      <Card.Title>{item?.name}</Card.Title>
+                      <Card.Text>Price $</Card.Text>
+                      <Button variant="primary">Add to cart</Button>
+                    </Card.Body>
+                  </Card>
                 </div>
-              </form>
-            </div>
-          </Carousel.Item>
-        </Carousel>))}
+                <div className="col-7">
+                  <Card style={{ width: "18rem", height: "400px" }}>
+                    <Card.Body>
+                      <Card.Title>{item?.name}</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">
+                        {item?.category}
+                      </Card.Subtitle>
+                      <Card.Text>Detalles de la obra</Card.Text>
+                      <Card.Link href="#">Card Link</Card.Link>
+                      <Card.Link href="#">Price: $ {item?.price}</Card.Link>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </div>
+              <div className="input-container">
+                <form action="">
+                  <Form.Label>Comment</Form.Label>
+                  <div className="row">
+                    <Form.Control
+                      type="text"
+                      placeholder="Leave your comments here please"
+                    />
+                    <Button variant="primary" type="submit">
+                      Submit
+                    </Button>
+                  </div>
+                </form>
+              </div>
+            </Carousel.Item>
+          </Carousel>
+        ))}
       </div>
       <div className="col-3 nav"></div>
       <div className="col-9 comments"></div>
