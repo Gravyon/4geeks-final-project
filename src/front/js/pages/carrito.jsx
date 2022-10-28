af3e19456c7751b55de28e3e5b5604418a0c9ae4
hermanbayron <foforito2001gg@gmail.com>
2022-10-28 20:54:40 +0000

    cambios en favorito y carrito

--- src/front/js/pages/carrito.jsx
+++ src/front/js/pages/carrito.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useContext, useRef } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export const Carrito = () => {
   const { store, actions } = useContext(Context);
@@ -14,39 +16,37 @@ export const Carrito = () => {
 
   return (
     <div className="container mt-5">
-      <Link
-        className="nav-link dropdown-toggle btn btn-danger text-white"
-        to="/"
-        role="button"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        Carrito {store.products.length}
-      </Link>
-      <ol>
-        <li>
-          <button className="dropdown-item">
-            {store.products.map((item, id) => (
-              <li className="list-group-item border border-1" key={id}>
-                {item.name}
-                <p>Precio: {item.price}</p>
-                <span
-                  className="close btn btn-danger"
-                  onClick={() => actions.removeProduct(item)}
-                >
-                  <b>X</b>
-                </span>
-              </li>
-            ))}
-          </button>
-        </li>
-      </ol>
+      <div className="w-25">
+        <h1>Carrito {store.products.length}</h1>
+      </div>
+      <div>
+        <ol>
+          <li>
+            <button className="dropdown-item">
+              {store.products.map((item, id) => (
+                <li className="list-group-item border border-1" key={id}>
+                  {item.name}
+                  <p>Precio: {item.price}</p>
+                  <span
+                    className="close btn btn-danger"
+                    onClick={() => actions.removeProduct(item)}
+                  >
+                    <b>X</b>
+                  </span>
+                </li>
+              ))}
+            </button>
+          </li>
+        </ol>
+      </div>
       {/* <p>Total: {item.price}</p> */}
       {/* <button className="btn btn-danger">
         <Link to={"/"}>Comprar</Link>
       </button> */}
-      <button className="btn btn-danger">
-        <Link to={"/"}>Home</Link>
+      <button className="btn btn-dark">
+        <Nav.Link className="text-warning" href="/">
+          Home
+        </Nav.Link>
       </button>
     </div>
   );
