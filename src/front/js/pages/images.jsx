9b3a36413a2ac6041b42b65dbefcee72213abd61
lauramagallanes <pasocenturiontours@gmail.com>
2022-10-28 18:28:39 +0000

    guardo cambios realizados en images.jsx y en flux

--- src/front/js/pages/images.jsx
+++ src/front/js/pages/images.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { FormGroup, Label, Input, FormText } from "reactstrap";
+import { FormGroup, Label, Input, FormText, Form, Button } from "reactstrap";
 import { useState } from "react";
 
 export const SubirImagenes = () => {
 
     const [image, setImage] = useState("");
     const [loading, setLoading] = useState(false);
+    const[name, setName] = useState("");
+    const[category, setCategory] = useState("");
+    const[description, setDescription] = useState("");
+    const[price, setPrice] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        let onUploaded = await actions.createProduct(name, description, category, url, price);
+        console.log(onUploaded)
+
+        setImage("")
+        setName("")
+        setCategory("")
+        setDescription("")
+        setPrice("")
+    }
 
     const uploadImage = async (e) => {
         const files = e.target.files;
@@ -30,6 +46,7 @@ export const SubirImagenes = () => {
     return (
         <div className="container mt-5">
             <h1>Upload your work of art</h1>
+            <Form onSubmit={handleSubmit}>
             <FormGroup>
                 <Label for="exampleName">
                     Name
@@ -64,10 +81,14 @@ export const SubirImagenes = () => {
                     type="file"
                     onChange={uploadImage}
                 />
+                <Button color="info" type="submit">
+    Submit
+  </Button>
                 <FormText>
                     This is some placeholder block-level help text for the above input. It‘s a bit lighter and easily wraps to a new line.
                 </FormText>
             </FormGroup>
+            </Form>
         </div>
     )
 }
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,123 +1,144 @@
 import axios from "axios";
 
-const getState = ({ getStore, getActions, setStore }) => {
-  return {
-    store: {
-      product: [],
-      productDetail: {},
-      products: [],
+const getState = ({
+    getStore,
+    getActions,
+    setStore
+}) => {
+    return {
+        store: {
+            product: [],
+            productDetail: {},
+            products: [],
 
-      listaFavoritos: [],
-      // listaCarrito: [],
+            listaFavoritos: [],
+            // listaCarrito: [],
 
-      auth: false,
-      register: false,
-    },
-    actions: {
-      // fecht de los cuadros
-      getProduct: async () => {
-        try {
-          const response = await fetch(
-            process.env.BACKEND_URL + "/api/product"
-          ); //ir a buscar
-          const data = await response.json();
-          console.log(data);
-          setStore({
-            product: data,
-          }); //promesa 2
-        } catch (err) {
-          // console.log(err);
-        }
+            auth: false,
+            register: false,
+        },
+        actions: {
+            // fecht de los cuadros
+            getProduct: async () => {
+                try {
+                    const response = await fetch(
+                        process.env.BACKEND_URL + "/api/product"
+                    ); //ir a buscar
+                    const data = await response.json();
+                    console.log(data);
+                    setStore({
+                        product: data,
+                    }); //promesa 2
+                } catch (err) {
+                    // console.log(err);
+                }
 
-        // fecht de los detalles
-      },
-      // funcion para obtener detalles de los cuadros
-      getProductDetail: async (id) => {
-        try {
-          const response = await fetch(
-            process.env.BACKEND_URL + "/api/product/" + id
-          );
-          const data = await response.json();
-          console.log(data);
+                // fecht de los detalles
+            },
+            // funcion para obtener detalles de los cuadros
+            getProductDetail: async (id) => {
+                try {
+                    const response = await fetch(
+                        process.env.BACKEND_URL + "/api/product/" + id
+                    );
+                    const data = await response.json();
+                    console.log(data);
 
-          setStore({
-            productDetail: data,
-          });
-        } catch (err) {
-          console.log(err);
-        }
-      },
-      // funcion para Login
-      login: async (email, password) => {
-        try {
-          const response = await axios.post(
-            process.env.BACKEND_URL + "/api/login",
-            {
-              email: email,
-              password: password,
+                    setStore({
+                        productDetail: data,
+                    });
+                } catch (err) {
+                    console.log(err);
+                }
+            },
+            // funcion para Login
+            login: async (email, password) => {
+                try {
+                    const response = await axios.post(
+                        process.env.BACKEND_URL + "/api/login", {
+                            email: email,
+                            password: password,
+                        }
+                    );
+
+                    localStorage.setItem("token", response.data.msg);
+                    console.log(response.data.msg);
+                    setStore({
+                        auth: true,
+                    });
+                    return true;
+                } catch (error) {
+                    console.log(error);
+                    if (error.code === "ERR_BAD_REQUEST") {
+                        alert(error.response.data.msg);
+                    }
+                }
+            },
+
+            // logout
+
+            logout: () => {
+                localStorage.removeItem("token");
+                setStore({
+                    auth: false,
+                });
+                return false;
+            },
+            getProducts: (product) => {
+                const store = getStore();
+                if (store.products.includes(product)) {
+                    getActions().removeProduct(product);
+                } else {
+                    setStore({
+                        products: [...store.products, product],
+                    });
+                }
+            },
+            removeProduct: (product) => {
+                const store = getStore();
+                setStore({
+                    products: store.products.filter((item) => item !== product),
+                });
+            },
+            // constante marcar favoritos
+            marcarFavoritos: (i) => {
+                let store = getStore();
+                if (store.listaFavoritos.includes(i)) {
+                    getActions().eliminarFavoritos(i);
+                } else {
+                    setStore({
+                        listaFavoritos: [...store.listaFavoritos, i],
+                    });
+                }
+            },
+
+            // constante eliminar favoritos
+            eliminarFavoritos: (i) => {
+                let store = getStore();
+                setStore({
+                    listaFavoritos: store.listaFavoritos.filter((item) => item !== i),
+                });
+            },
+
+            // funcion para crear productos
+
+            createProduct: async (name, description, category, url, price) => {
+                try {
+
+                    const response = await axios.post("url-de-crear-producto", {
+                        name: name,
+                        description: description,
+                        category: category,
+                        url: url,
+                        price: price
+                    })
+
+                } catch (error) {
+                    console.log(error)
+                }
             }
-          );
-
-          localStorage.setItem("token", response.data.msg);
-          console.log(response.data.msg);
-          setStore({
-            auth: true,
-          });
-          return true;
-        } catch (error) {
-          console.log(error);
-          if (error.code === "ERR_BAD_REQUEST") {
-            alert(error.response.data.msg);
-          }
-        }
-      },
-
-      // logout
-
-      logout: () => {
-        localStorage.removeItem("token");
-        setStore({
-          auth: false,
-        });
-        return false;
-      },
-      getProducts: (product) => {
-        const store = getStore();
-        if (store.products.includes(product)) {
-          getActions().removeProduct(product);
-        } else {
-          setStore({
-            products: [...store.products, product],
-          });
-        }
-      },
-      removeProduct: (product) => {
-        const store = getStore();
-        setStore({
-          products: store.products.filter((item) => item !== product),
-        });
-      },
-      // constante marcar favoritos
-      marcarFavoritos: (i) => {
-        let store = getStore();
-        if (store.listaFavoritos.includes(i)) {
-          getActions().eliminarFavoritos(i);
-        } else {
-          setStore({
-            listaFavoritos: [...store.listaFavoritos, i],
-          });
-        }
-      },
-
-      // constante eliminar favoritos
-      eliminarFavoritos: (i) => {
-        let store = getStore();
-        setStore({
-          listaFavoritos: store.listaFavoritos.filter((item) => item !== i),
-        });
-      },
-    },
-  };
+        },
+    };
 };
 
 export default getState;
