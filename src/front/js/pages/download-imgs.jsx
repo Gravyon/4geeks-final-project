f0583917e7370cdfb330e8cc395698587a8597b4
lauramagallanes <pasocenturiontours@gmail.com>
2022-10-28 14:24:20 +0000

    actualizacion previo a pull

--- src/front/js/pages/download-imgs.jsx
+++ src/front/js/pages/download-imgs.jsx
@@ -16,7 +16,7 @@ export const DownloadImages = (props) => {
         // const files = e.target.files;
         const results = await fetch("https://api.cloudinary.com/v1_1/"+ process.env.CLOUDINARY_CLOUD_NAME +"/resources/image", {
             headers: {
-                // Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_API_KEY + ":" + process.env.CLOUDINARY_API_SECRET).toString('base64')}`
+                Authorization: `Basic ${btoa(process.env.CLOUDINARY_API_KEY + ":" + process.env.CLOUDINARY_API_SECRET)}`
             }
         })
         .then((response)=>response.json())
