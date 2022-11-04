31d98dd71b8ddc6ab2aebc79674493ff2ec21d23
lauramagallanes <pasocenturiontours@gmail.com>
2022-11-04 15:03:12 +0000

    cambios previos al pull

--- src/front/js/component/checkout-paypal.jsx
+++ src/front/js/component/checkout-paypal.jsx
@@ -3,42 +3,62 @@ import ReactDOM, { render } from "react-dom";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { Context } from "../store/appContext";
 
-// const { store, actions } = useContext(Context);
-
-
 export const PayPalCheckout = () => {
+  const { actions, store } = useContext(Context);
+  const [price, setPrice] = useState([]);
+  const [suma, setSuma] = useState(0);
 
-  // const [price, setPrice] = useState(0);
-  // setPrice(store.shoppingList.map((item) => suma =+ item.price))
-  // console.log(price)
+  useEffect(() => {
+    if (store.userId != null) {
+      // console.log(store.userId)
+      //   actions.getShopping();
+      sumarTotal();
+    }
+  }, [store.userId]);
 
-  // //funcion para crear orden:
+  console.log(store.shoppingList);
 
-  // const createOrder = (data, actions) => {
-  //   return actions.order.create({})
-  // }
-  
+  const sumarTotal = () => {
+    setPrice(
+      store.shoppingList.map((item) => {
+        item.price;
+        return item.price;
+      })
+    );
+    console.log(sumarTotal); //price es mi nuevo array
+
+    function sumar(item) {
+      setSuma(suma + item);
+      return suma;
+    }
+
+    price.forEach(sumar);
+    console.log(suma);
+  };
+
   return (
-    <PayPalScriptProvider options={{"client-id": process.env.PAYPAL_SANDBOX_CLIENT_ID}}>
-            <PayPalButtons 
-            createOrder={(data, actions) => {
-              return actions.order.create({
-                  purchase_units: [
-                      {
-                          amount: {
-                              value: "1.99",
-                          },
-                      },
-                  ],
-              });
-          }}
-          onApprove={(data, actions) => {
-              return actions.order.capture().then((details) => {
-                  const name = details.payer.name.given_name;
-                  alert(`Transaction completed by ${name}`);
-              });
-          }}
-            />
-        </PayPalScriptProvider>
-)
+    <PayPalScriptProvider
+      options={{ "client-id": process.env.PAYPAL_SANDBOX_CLIENT_ID }}
+    >
+      <PayPalButtons
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: "1.99",
+                },
+              },
+            ],
+          });
+        }}
+        onApprove={(data, actions) => {
+          return actions.order.capture().then((details) => {
+            const name = details.payer.name.given_name;
+            alert(`Transaction completed by ${name}`);
+          });
+        }}
+      />
+    </PayPalScriptProvider>
+  );
 };
