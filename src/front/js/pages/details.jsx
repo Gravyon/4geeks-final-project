5b95049902ed9c32e0997204655bf7b507744bc7
lauramagallanes <pasocenturiontours@gmail.com>
2022-11-10 13:37:58 +0000

    barra de busqueda responsive

--- src/front/js/pages/details.jsx
+++ src/front/js/pages/details.jsx
@@ -14,6 +14,7 @@ import {
   CarouselControl,
   CarouselCaption,
 } from "reactstrap";
+import "../../styles/details.css";
 
 export const ProductDetail = (props) => {
   const { store, actions } = useContext(Context);
@@ -75,6 +76,45 @@ export const ProductDetail = (props) => {
                   >
                     {item.description}
                   </p>
+                  <form>
+                    <p className="clasificacion">
+                      <input
+                        id="radio1"
+                        type="radio"
+                        name="estrellas"
+                        value="5"
+                      />
+                      <label for="radio1">★</label>
+                      <input
+                        id="radio2"
+                        type="radio"
+                        name="estrellas"
+                        value="4"
+                      />
+                      <label for="radio2">★</label>
+                      <input
+                        id="radio3"
+                        type="radio"
+                        name="estrellas"
+                        value="3"
+                      />
+                      <label for="radio3">★</label>
+                      <input
+                        id="radio4"
+                        type="radio"
+                        name="estrellas"
+                        value="2"
+                      />
+                      <label for="radio4">★</label>
+                      <input
+                        id="radio5"
+                        type="radio"
+                        name="estrellas"
+                        value="1"
+                      />
+                      <label for="radio5">★</label>
+                    </p>
+                  </form>
                 </div>
                 <div className="card-footer align-bottom ">
                   <h6 className="card-text">Price: USD {item.price}</h6>
