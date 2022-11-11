66f736675fa98f77baca4b8bc78224bdfdd35dd1
aucapricornus <rdjmartinez95@gmail.com>
2022-11-11 14:57:04 +0000

    Contact us no anda con formik por ahora

--- src/front/js/pages/contactus.jsx
+++ src/front/js/pages/contactus.jsx
@@ -1,97 +1,117 @@
-import React, { useState, useContext, useEffect } from "react";
-import { Context } from "../store/appContext";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { Component } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import { useForm, ValidationError } from "@formspree/react";
+import React from "react";
+import { useForm} from "@formspree/react";
 import Iframe from "react-iframe";
-
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 export const ContactUs = () => {
-  // definimos los estados
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const { actions } = useContext(Context);
-  let navigate = useNavigate();
 
-  const [state, handleSubmit] = useForm(process.env.CONTACT_FORM);
-  if (state.succeeded) {
-    return (
-      <h1 className="position-relative position-absolute top-50 start-50 translate-middle">
-        Thanks for your feedback!
-      </h1>
-    );
-  }
+  const SignupSchema = Yup.object().shape({
+    fname: Yup.string('Enter your name')
+    .max(50, "Too Long!")
+    .min(2, 'Too short')
+    .required("A name is required"),
+    lname: Yup.string()
+    .min(2, 'Too short')
+    .max(50, "Too Long!"),
+    email: Yup.string('Enter your email')
+      .email('Enter a valid email')
+      .required("Email required"),
+    message: Yup.string()
+    .min(2, 'Too short')
+    .max(250, "Too Long!")
+    .required("Please leave your feedback"),
+  });
+
+// const contactForm = (values) => {
+//   const [state] = useForm(process.env.CONTACT_FORM);
+//   if (state.succeeded) {
+//     return (
+//       <h1 className="position-relative position-absolute top-50 start-50 translate-middle">
+//         Thanks for your feedback!
+//       </h1>
+//     );
+//   }
+// }
+
   return (
-    <form onSubmit={handleSubmit}>
+    <Formik
+    initialValues={{ email: "", fname: "", lname:"", message:"" }}
+    validationSchema={SignupSchema}
+    onSubmit={(values) => {
+      values.email
+      values.fname
+      values.lname
+      values.message
+      setTimeout(() => {
+        console.log(JSON.stringify(values, null, 2));
+        // setSubmitting(false);
+      }, 400);
+      const [state] = useForm(process.env.CONTACT_FORM);
+      if (state.succeeded) {
+        return (
+          <h1 className="position-relative position-absolute top-50 start-50 translate-middle">
+            Thanks for your feedback!
+          </h1>
+        );
+      }
+    }}
+    >
+    {({ errors, touched}) => (
+    <Form>
       <div className="container-fluid">
         <div className="row">
           <div className="col-12 mb-3">
             <div
               className="card bg-dark text-white my-5 mx-auto"
-              style={{ borderRadius: "1rem", maxWidth: "800px" }}
-            >
+              style={{ borderRadius: "1rem", maxWidth: "900px" }}
+              >
               <div className="card-body p-5 d-flex flex-column align-items-center mx-auto w-100">
                 <h2 className="fw-bold mb-2 text-uppercase" style={{ color: "#bdb284" }}>Contact Us</h2>
                 <p className="text-white-50">
                   Please enter your info and we will contact you
                 </p>
                 <div className="col-12 p-3">
-                  <input
+                  <Field
                     className="form-control"
-                    id="formControlLg"
+                    name="fname"
                     type="text"
-                    size="lg"
-                    onChange={(e) => setFirstName(e.target.value)}
-                    value={firstName}
-                    required
-                    
                     placeholder="First Name"
-                  />
+                    />
+                    {errors.fname && touched.fname && errors.fname}
                 </div>
                 <div className="col-12 p-3">
-                  <input
-                    id="formControlLg"
+                  <Field
+                    name="lname"
                     className="form-control"
                     type="text"
-                    size="lg"
-                    onChange={(e) => setLastName(e.target.value)}
-                    value={lastName}
-                    required
                     placeholder="Last Name"
                   />
+                  {errors.lname && touched.lname && errors.lname}
                 </div>
                 <div className="col-12 p-3">
-                  <input
-                    id="formControlLg"
+                  <Field
+                    name="email"
                     className="form-control"
                     type="email"
-                    size="lg"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                    required
                     placeholder="Email Address"
-                  />
+                    />
+                    {errors.email && touched.email && errors.email}
                 </div>
                 <div className="col-12 form-group shadow-textarea p-3">
-                  <textarea
+                  <Field
                     className="form-control z-depth-1"
-                    id="message"
+                    type="text"
+                    component="textarea"
                     name="message"
                     rows="3"
                     placeholder="Write something here..."
-                    required
-                    onChange={(e) => setMessage(e.target.value)}
-                    value={message}
-                  ></textarea>
+                  />
+                  {errors.message && touched.message && errors.message}
                   <br />
                 </div>
                 <button
-                  type="submit "
+                  type="submit"
                   className="btn btn-outline-light btn-lg mx-2 px-5" style={{ color: "#bdb284" }}
                   color="white"
                 >
@@ -111,6 +131,8 @@ export const ContactUs = () => {
           </div>
         </div>
       </div>
-    </form>
+    </Form>
+    )}
+</Formik>
   );
 };
--- src/front/js/pages/signup.jsx
+++ src/front/js/pages/signup.jsx
@@ -14,121 +14,118 @@ export const SignUp = () => {
   let navigate = useNavigate();
 
   const SignupSchema = Yup.object().shape({
-    email: Yup.string('Enter your email')
-      .email('Enter a valid email')
+    email: Yup.string("Enter your email")
+      .email("Enter a valid email")
       .required("Email required"),
-    password: Yup.string('Enter your password')
-      .min(2, 'Password should be of minimum 8 characters length')
+    password: Yup.string("Enter your password")
+      .min(2, "Password should be of minimum 8 characters length")
       .max(50, "Too Long!")
       .required("Password required"),
-    username: Yup.string('Enter your username')
-    .min(2, 'Username should be of minimum 8 characters length')
-    .max(30, "Too Long!")
-    .required("Username required"),
+    username: Yup.string("Enter your username")
+      .min(2, "Username should be of minimum 8 characters length")
+      .max(30, "Too Long!")
+      .required("Username required"),
   });
-  // const doSubmit = async (e) => {
-  //   e.preventDefault();
-  //   let onSignUp = await actions.signup(username, email, password);
-
-  //   if (onSignUp === "User email already exists") {
-  //     swal("User email already exists, redirecting to login");
-  //     navigate("/login");
-  //   } else if (onSignUp === "New user created") {
-  //     navigate("/");
-  //   }
-  // };
-
   return (
     <Formik
-    //Valores iniciales
-    initialValues={{ email: "", password: "", username: "" }}
-    validationSchema={SignupSchema}
-    // Declara onSubmit y se le pasan los valores del login dentro, anotandolos con values
-    onSubmit={async (values) => {
-      let onSignUp = await actions.signup(values.email, values.password, values.username);
-      if (onSignUp === "User email already exists") {
-        swal("User email already exists, redirecting to login");
-        navigate("/login");
-      } else if (onSignUp === "New user created") {
-        navigate("/");
-      }
-    }}
-  >
-      {({ errors, touched}) => (
-     <Form>
-       <div className="container-fluid text-center">
-         <div className="row d-flex justify-content-center align-items-center h-100">
-           <div col="12">
-             <div
-              className="bg-dark text-white my-5 mx-auto"
-              style={{ borderRadius: "1rem", maxWidth: "400px" }}
-            >
-              <div className="p-5 d-flex flex-column align-items-center mx-auto w-100">
-                <h2
-                  className="fw-bold mb-2 text-uppercase"
-                  style={{ color: "#bdb284" }}
+      //Valores iniciales
+      initialValues={{ email: "", password: "", username: "" }}
+      validationSchema={SignupSchema}
+      // Declara onSubmit y se le pasan los valores de cada campo, anotandolos con values
+      onSubmit={async (values) => {
+        let onSignUp = await actions.signup(
+          values.email,
+          values.password,
+          values.username
+        );
+        if (onSignUp === "User email already exists") {
+          swal("User email already exists, redirecting to login");
+          navigate("/login");
+        } else if (onSignUp === "New user created") {
+          navigate("/");
+        }
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <div className="container-fluid text-center">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div col="12">
+                <div
+                  className="bg-dark text-white my-5 mx-auto"
+                  style={{ borderRadius: "1rem", maxWidth: "400px" }}
                 >
-                  Register
-                </h2>
-                <p className="mt-3 mb-3" style={{ color: "#bdb284" }}>
-                  Already registered?{" "}
-                  <Link to="/login" className="text-white-50 fw-bold">
-                    Login
-                  </Link>
-                </p>
-    
-                <div className="col-12 ">
-                  <Field
-                    type="text"
-                    name="username"
-                    placeholder="Username"
-                    className="form-control"
-                  />
-                  {errors.username && touched.username && errors.username}
-                  <label className="form-label" htmlFor="form1Example2"></label>
+                  <div className="p-5 d-flex flex-column align-items-center mx-auto w-100">
+                    <h2
+                      className="fw-bold mb-2 text-uppercase"
+                      style={{ color: "#bdb284" }}
+                    >
+                      Register
+                    </h2>
+                    <p className="mt-3 mb-3" style={{ color: "#bdb284" }}>
+                      Already registered?{" "}
+                      <Link to="/login" className="text-white-50 fw-bold">
+                        Login
+                      </Link>
+                    </p>
+
+                    <div className="col-12 ">
+                      <Field
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        className="form-control"
+                      />
+                      {errors.username && touched.username && errors.username}
+                      <label
+                        className="form-label"
+                        htmlFor="form1Example2"
+                      ></label>
+                    </div>
+                    <div className="col-12 ">
+                      <Field
+                        type="email"
+                        name="email"
+                        placeholder="Email Address"
+                        className="form-control"
+                      />
+                      {errors.email && touched.email && errors.email}
+                      <label
+                        className="form-label"
+                        htmlFor="form1Example2"
+                      ></label>
+                    </div>
+                    <div className="col-12 ">
+                      <Field
+                        type="password"
+                        name="password"
+                        className="form-control"
+                        placeholder="Password"
+                      />
+                      {errors.password && touched.password && errors.password}
+                    </div>
+                    <p className="small mb-3 pb-lg-2">
+                      <br />
+                      <Link className="text-white-50" to={"/changePassword"}>
+                        Forgot password?
+                      </Link>
+                    </p>
+
+                    <button
+                      type="submit "
+                      className="btn btn-outline-light btn-lg mx-2 px-5"
+                      style={{ color: "#bdb284" }}
+                      color="white"
+                    >
+                      Signup
+                    </button>
+                  </div>
                 </div>
-                <div className="col-12 ">
-                  <Field
-                    type="email"
-                    name="email"
-                    placeholder="Email Address"
-                    className="form-control"
-                  />
-                  {errors.email && touched.email && errors.email}
-                  <label className="form-label" htmlFor="form1Example2"></label>
-                </div>
-                <div className="col-12 ">
-                  <Field
-                    type="password"
-                    name="password"
-                    className="form-control"
-                    placeholder="Password"
-                  />
-                  {errors.password && touched.password && errors.password}
-                </div>
-                <p className="small mb-3 pb-lg-2">
-                  <br />
-                  <Link className="text-white-50" to={"/changePassword"}>
-                    Forgot password?
-                  </Link>
-                </p>
-    
-                <button
-                  type="submit "
-                  className="btn btn-outline-light btn-lg mx-2 px-5"
-                  style={{ color: "#bdb284" }}
-                  color="white"
-                >
-                  Signup
-                </button>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </Form>
-    )}
+        </Form>
+      )}
     </Formik>
-    );
-  };
-  
+  );
+};
