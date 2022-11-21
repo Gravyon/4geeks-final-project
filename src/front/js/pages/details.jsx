4a0bea2f2a27081b111750fd7c83819859f02034
lauramagallanes <pasocenturiontours@gmail.com>
2022-11-21 19:54:58 +0000

    commit previo al pull de daniel

--- src/front/js/pages/details.jsx
+++ src/front/js/pages/details.jsx
@@ -261,7 +261,7 @@ export const ProductDetail = () => {
                   </div>
                   <div className="card-footer align-bottom h-50 ">
                     <div className="buttonsCarritoYFavorito">
-                      <div className="d-md-grid gap-md-2 d-sm-flex justify-sm-conten-between">
+                      <div className="d-md-grid gap-md-2">
                         <button
                           className="btn btn-light d-flex justify-content-sm-between justify-content-md-center btn-sm-sm btnCarrito"
                           type="button"
